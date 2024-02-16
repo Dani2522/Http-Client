82fb5392dac8553f8252aed5d18610d2457b30ba
danielamoreno <lauritamobu@gmail.com>
2024-02-15 20:58:42 -0500

    primer commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { AppService } from './app.service';
+import { Answer } from './models/answer.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  pregunta = '';
+  respuesta?: Answer;
+
+  constructor(private appService: AppService) {}
+
+  onSubmit() {
+    this.appService.obtenerRespuesta().subscribe(
+      (res) => {   // Especifica el tipo de 'res' como Answer
+        this.respuesta = res;
+      }
+      );
+  }
+}
